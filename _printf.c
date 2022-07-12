3c0b7c3e23395d51acf9263606abedce510636f3
Sophie Akpa <akpasophie@gmail.com>
2022-07-12 15:11:11 -0700

    added files

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _printf - This functions prints the (“character, 
+ * string, float, integer, octal and hexadecimal values”) 
+ * onto the output screen
+ *
+ * @format: the format of the characters
+ * @
+ */
+
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+}
