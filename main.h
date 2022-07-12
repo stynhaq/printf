be0e629d3cf72f4dade740503058018fb65ccc5e
Sophie Akpa <akpasophie@gmail.com>
2022-07-12 13:01:57 -0700

    added files

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <limits.h>
+
+#define BUFFSIZE 1024
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
